29b715930ed066d1a0b0f7372ba6767f8ca14a55
Anazodo Vanessa <anazodovanessa19@gmail.com>
2023-04-17 15:30:03 +0100

    init

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 const Para = document.getElementById('para');
 const email = document.querySelector('.email-address');
 const btnSubmit = document.querySelector('.btn-submit');
+const errorImage = document.querySelector('.alert-icon');
 
 
 btnSubmit.addEventListener('click', (e)=>{
     e.preventDefault();
     var mail_format = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     if(email.value.match(mail_format)){
-        Para.innerHTML = 'Thank you. Check your email for update';
+        Para.innerHTML = 'Thank you for subscribing';
         Para.style.color = 'green';
+        email.style.border = '1px solid black';
+        errorImage.style.display = 'none';
+
     }
     else{
         Para.innerHTML = 'Please provide a valid email';
         Para.style.color = 'red';
+        email.style.border = '1px solid red';
+        errorImage.style.display = 'block';
+
+
+
+        
     }
 });
